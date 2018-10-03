605cf24daffac96d7f49f628b7d793ffb649569a
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-10-03 19:30:23 +1000

    Generating third use case

--- test/hotel/checkout/CheckoutCTLTest.java
+++ test/hotel/checkout/CheckoutCTLTest.java
@@ -7,6 +7,8 @@ package hotel.checkout;
 
 import hotel.HotelHelper;
 import hotel.entities.Hotel;
+import hotel.entities.ServiceType;
+import hotel.service.RecordServiceCTL;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.After;
@@ -65,6 +67,7 @@ public class CheckoutCTLTest {
     
       /**
      * Test of roomIdEntered method, of class CheckoutCTL.
+     * @throws java.lang.Exception
      */
     @Test
     public void testRoomIdEnteredTwoActiveService() throws Exception {
@@ -84,5 +87,34 @@ public class CheckoutCTLTest {
         double actualTotal = Double.parseDouble(actualTotalString);
         assertEquals(total, actualTotal, 0);
     }
+    
+    /**
+     * Test of roomIdEntered method, of class CheckoutCTL.
+     */
+    @Test
+    public void testRoomIdEnteredthreeActiveService() throws Exception {
+        int roomNumber = 301;
+        double costRoomService = 7.00;
+        double costBarFridge = 10.0;
+        double costRestaurant = 20.0;
+        double total = costRoomService + costBarFridge + costRestaurant;
+        Hotel hotel = HotelHelper.loadHotel();
+        
+        RecordServiceCTL recordService = new RecordServiceCTL(hotel);
+        recordService.roomNumberEntered(roomNumber);
+        recordService.serviceDetailsEntered(ServiceType.BAR_FRIDGE, costBarFridge );
+        
+        recordService = new RecordServiceCTL(hotel);
+        recordService.roomNumberEntered(roomNumber);
+        recordService.serviceDetailsEntered(ServiceType.RESTAURANT, costRestaurant );
+        
+        CheckoutCTL instance = new CheckoutCTL(hotel);
+        instance.roomIdEntered(roomNumber);
+        String actualOutput = outContent.toString();
+        String actualTotalString = actualOutput.substring(actualOutput.indexOf("Total")+ 8);
+        double actualTotal = Double.parseDouble(actualTotalString);
+        assertEquals(total, actualTotal, 0);
+    }
+
     
 }
