d693308fe9e068a49cbca88909bd952c56b96c67
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-10-03 18:50:06 +1000

    Generating second use case

--- test/hotel/checkout/CheckoutCTLTest.java
+++ test/hotel/checkout/CheckoutCTLTest.java
@@ -63,4 +63,26 @@ public class CheckoutCTLTest {
         assertEquals(total, actualTotal, 0);
     }
     
+      /**
+     * Test of roomIdEntered method, of class CheckoutCTL.
+     */
+    @Test
+    public void testRoomIdEnteredTwoActiveService() throws Exception {
+        int roomNumber = 301;
+        double costRoomService = 7.00;
+        double costBarFridge = 10.0;
+        double total = costRoomService + costBarFridge;
+        Hotel hotel = HotelHelper.loadHotel();
+        RecordServiceCTL recordService = new RecordServiceCTL(hotel);
+        recordService.roomNumberEntered(roomNumber);
+        recordService.serviceDetailsEntered(ServiceType.BAR_FRIDGE, costBarFridge );
+        
+        CheckoutCTL instance = new CheckoutCTL(hotel);
+        instance.roomIdEntered(roomNumber);
+        String actualOutput = outContent.toString();
+        String actualTotalString = actualOutput.substring(actualOutput.indexOf("Total")+ 8);
+        double actualTotal = Double.parseDouble(actualTotalString);
+        assertEquals(total, actualTotal, 0);
+    }
+    
 }
